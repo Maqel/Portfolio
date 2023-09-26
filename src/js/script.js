fa2dd3e3d3af1e8ba78b73a1d9eaef8f4f4f89c9
Maqel <98158452+Maqel@users.noreply.github.com>
2023-09-26 16:17:37 +0200

    working_nav

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,16 @@
 const toggleBtn = document.querySelector('.toggle_btn');
+const dropDownMenu = document.querySelector('.dropdown_menu');
+
 let menuOpen = false;
 toggleBtn.addEventListener('click', () => {
   if(!menuOpen) {
     toggleBtn.classList.add('open');
+    dropDownMenu.classList.add('open');
     menuOpen = true;
   } else {
     toggleBtn.classList.remove('open');
+    dropDownMenu.classList.remove('open');
+
     menuOpen = false;
   }
 });
